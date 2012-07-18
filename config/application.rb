14b643c3edd0ccbe27b2e2cde3ea3ec75060e651
MarconiVini <cardosounicamp@gmail.com>
2012-07-18 08:11:15 -0300

    Ajustes de layout, botao expansivel traducao de mensagens de erros

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module PersonalTrainer
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
-    config.time_zone = 'Brasilia'
+    #config.time_zone = 'Brasilia'
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
