2908528e8634ba6918fd6eaee6895163d0ca695b
MarconiVini <cardosounicamp@gmail.com>
2012-07-19 00:28:20 -0300

    finalizado a traducao dos erros e dos timers..add traducao em textos e formato de data

--- config/application.rb
+++ config/application.rb
@@ -30,15 +30,16 @@ module PersonalTrainer
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    #config.i18n.default_locale = :pt
-    #config.i18n.locale = :pt
-    I18n.default_locale = :pt
+    config.time_zone = 'Brasilia'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = :"pt-BR"
+    config.i18n.default_locale = :"pt-BR"
+    
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
-
-    #config.time_zone = 'Brasilia'
-
+    
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
